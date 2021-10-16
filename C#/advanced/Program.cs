5dc6c4cc236247c495e446db14dc4714b452be8e
caonhatduc9 <caonhatduc9@gmail.com>
2021-10-16 11:21:23 +0700

    tai len web demo

--- C#/advanced/Program.cs
+++ C#/advanced/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Drawing;
+using System.Net.Http;
+using System.Reflection.PortableExecutable;
+using System.IO;//lam viec voi o dia
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +14,106 @@ namespace advanced
 {
     public delegate void showLog(string msg);
     static class Abc
+    {
+        public static void PrintEx(this string s, ConsoleColor c)
         {
-            public static void PrintEx(this string s, ConsoleColor c)
+            Console.ForegroundColor = c;
+            Console.WriteLine(s);
+        }
+    }
+    class PhanSo
+    {
+        private int tu;
+        private int mau;
+        public PhanSo(int tu, int mau)
+        {
+            this.tu = tu;
+            this.mau = mau;
+        }
+        public void show()
+        {
+            Console.WriteLine($"{tu}/{mau}");
+        }
+        //phanso <= phanso + phanSo
+        public static PhanSo operator +(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.tu * b.mau + b.tu * a.mau, a.mau * b.mau);
+        }
+        public static PhanSo operator +(PhanSo a, int b)
+        {
+            return new PhanSo(a.tu + b * a.mau, a.mau);
+        }
+        public static PhanSo operator +(int b, PhanSo a)
+        {
+            return new PhanSo(a.tu + b * a.mau, a.mau);
+        }
+        //indexer
+        public int this[int i]
+        {
+            set
             {
-                Console.ForegroundColor = c;
-                Console.WriteLine(s);
+                if (i == 0)
+                    tu = value;
+                else if (i == 1)
+                    mau = value;
+                else
+                    throw new Exception("sai index");
+            }
+            get
+            {
+                if (i == 0)
+                    return tu;
+                else if (i == 1)
+                    return mau;
+                else
+                    throw new Exception("sai index");
             }
         }
+    }
+ class Product
+        {
+            public int id { set; get; }
+            public double price { set; get; }
+            public string name { set; get; }
+       public  void save(Stream stream)
+        {
+            //int -> 4byte
+            var byteId = BitConverter.GetBytes(id);
+            stream.Write(byteId, 0, sizeof(int));
+            var bytePrice = BitConverter.GetBytes(price);
+            stream.Write(bytePrice, 0, sizeof(double));
+            var byteName = Encoding.UTF8.GetBytes(name);
+            var byteLength = BitConverter.GetBytes(byteName.Length);
+            stream.Write(byteLength, 0, byteLength.Length);
+            stream.Write(byteName, 0, byteName.Length);
+
+        }
+       public void restore(Stream stream)
+            {
+            var byteId = new byte[4];
+            stream.Read(byteId, 0, 4);
+            id = BitConverter.ToInt32(byteId, 0); 
+
+            var bytePrice = new byte[8];
+            stream.Read(bytePrice, 0, 8);
+            price = BitConverter.ToDouble(bytePrice, 0);
+
+            var byteLeng = new byte[4];
+            stream.Read(byteLeng, 0, 4);
+            int leng = BitConverter.ToInt32(byteLeng, 0);
+            var byteName = new byte[leng];
+            stream.Read(byteName, 0, leng);
+            name = Encoding.UTF8.GetString(byteName, 0, leng);
+
+
+
+        }
+        }
+
     class Program
     {
         //generic giong template trong C++
-#region something
+        #region something
         static void swap<DUC>(ref DUC a, ref DUC b)
         {
             DUC t = a;
@@ -73,9 +166,9 @@ namespace advanced
             // Console.Write (a);
             int year = 2022;
             string s = $"happy new year {year}";
-            Console.WriteLine (s);
+            Console.WriteLine(s);
         }
-#endregion
+        #endregion
 
 
         //kieu vo danh anonymous chi doc dell cho ghi
@@ -157,11 +250,12 @@ namespace advanced
             Console.WriteLine(s);
             Console.ResetColor();
         }
-        static void testDelegate(){
+        static void testDelegate()
+        {
             showLog log = null;
-         //   log = infor;
+            //   log = infor;
             // cach 1
-         //   log("\n\tDUC CAO");
+            //   log("\n\tDUC CAO");
             //cach 2 de thi hanh
             log?.Invoke("invoke DUC CAO");
         }
@@ -174,7 +268,111 @@ namespace advanced
             string s = " CAO NHAt DUC";
             s.PrintEx(ConsoleColor.Red);
         }
-        static void Main(string[] args)
+        //test qua tai toan tu
+        static void testOperator()
+        {
+            PhanSo a = new PhanSo(1, 2);
+            PhanSo b = new PhanSo(3, 4);
+            PhanSo c = new PhanSo(3, 4);
+            c = 1 + b + 1;
+            c.show();
+
+        }
+        static void testIndexer()
+        {
+            PhanSo a = new PhanSo(1, 2);
+            PhanSo b = new PhanSo(3, 4);
+            a[0] = 9;
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[1]);
+            Console.WriteLine(a[3]);
+        }
+        //test o dia ghi doc file
+        static void testDrive()
+        {
+            //   DriveInfo drive = new DriveInfo("Z");
+            var drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                Console.WriteLine($"name drive: {drive.Name}");
+                Console.WriteLine($"Drive Type: {drive.DriveType}");
+                Console.WriteLine($"label drive: {drive.VolumeLabel}");
+                Console.WriteLine($"format drive: {drive.DriveFormat}");
+                Console.WriteLine($"size drive: {drive.TotalSize}");
+                Console.WriteLine($"free drive: {drive.TotalFreeSpace}");
+                Console.WriteLine("-------------------");
+
+            }
+
+
+        }
+        static void testDirectory()
+        {
+            string path = "testDirectory";
+            // tao thu muc
+            Directory.CreateDirectory(path);
+            if (Directory.Exists(path))
+                Console.WriteLine(path + " ton tai");
+            else
+                Console.WriteLine(path + " khong ton tai");
+            // lay cac file ben trong thu muc
+            var files = Directory.GetFiles("obj");
+            foreach (var f in files)
+            {
+                Console.WriteLine(f);
+            }
+            // noi chuoi duong dan thu muc
+            Console.WriteLine(Path.DirectorySeparatorChar);
+            var path1 = Path.Combine("d1", "d2", "duc.txt");
+            // doi phan duoi file
+            path1 = Path.ChangeExtension(path1, "doc");
+            Console.WriteLine(path1);
+            Console.WriteLine(Path.GetFullPath(path1));
+
+        }
+        static void testFile()
+        {
+            string fileName = "duc.txt";
+            string content = " TRan Thao Quyen";
+            File.WriteAllText(fileName, content);
+            // them noi dung vao file khonflam mat noi dung cu
+            File.AppendAllText(fileName, content);
+        }
+        static void testFileStream()
+        {
+          //  string path1 = "duc.txt";
+          // using var stream = new FileStream(path: path1, FileMode.Open);
+          //luu tru
+          // byte[] buffer = { 12, 13, 14 };
+          // int offset = 0;
+          // int count = 3;
+          // stream.Write(buffer, 0, 3);
+          // //doc file
+          // int soByteDocDuoc = stream.Read(buffer, offset, count);
+          // //convert int,double -> byte
+          // int i = 1;
+          // var byteI = BitConverter.GetBytes(i);
+          // //byte to double,int
+          // BitConverter.ToInt32(byteI, 0);
+          // //string to byte
+          // string s = "cao nhat duc";
+          // var byteString = Encoding.UTF8.GetBytes(s);
+          // //byte to string
+          // Encoding.UTF8.GetString(byteString, 0, 15);
+               string path1 = "duc.txt";
+             using var stream = new FileStream(path: path1, FileMode.Open);
+            Product p = new Product();
+            // {
+            //     id = 10,
+            //     price = 10000,
+            //     name = "iphone 13 promax"
+            // };
+             //    p.save(stream);
+            
+            p.restore(stream);
+            Console.WriteLine($"id: {p.id}\nprice: {p.price}\nname: {p.name}");
+        }
+static void Main(string[] args)
         {
             // testMultilThread();
             // testString();
@@ -183,8 +381,13 @@ namespace advanced
             // cat.showInfor();
             //  anonymous();
             // testDelegate();
-            testExtensionMethod();
-
+            //testExtensionMethod();
+            // testOperator();
+            //testIndexer();
+            //testDrive();
+            // testDirectory();
+            //testFile();
+            testFileStream();
         }
     }
 }
---
+++ In-Class/Day1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Soldier
+    {
+        private int _hit;
+
+        private int _mana;
+
+        public Soldier()
+        {
+            _hit = 0;
+            _mana = 0;
+        }
+
+        public Soldier(int hitt, int manaa)
+        {
+            _hit = hitt;
+            _mana = manaa;
+        }
+
+        public int _Hit
+        {
+            get
+            {
+                return _hit;
+            }
+            set
+            {
+                _hit = value;
+            }
+        }
+
+        public int _Mana
+        {
+            get
+            {
+                return _mana;
+            }
+            set
+            {
+                _mana = value;
+            }
+        }
+    }
+
+    // class Fraction
+    // {
+    //     private float _numberator;
+    //     private float _denominator;
+    //     public Fraction()
+    //     {
+    //         _numberator = 0;
+    //         _denominator = 1;
+    //     }
+    //     public Fraction(float num, float denom)
+    //     {
+    //         _numberator = num;
+    //         _denominator = denom;
+    //     }
+    //     public void setNum(float num)
+    //     {
+    //         _numberator = num;
+    //     }
+    //     public void setDenom(float denom)
+    //     {
+    //         _denominator = denom;
+    //     }
+    //     public float getNum()
+    //     {
+    //         return _numberator;
+    //     }
+    //     public float getDenom()
+    //     {
+    //         return _denominator;
+    //     }
+    // }
+    // class Point
+    // {
+    //    private float          _x;
+    //      private float        _y;
+    //     public Point()
+    //     {
+    //         _x = 0;
+    //         _y = 0;
+    //     }
+    //     public Point(float x, float y)
+    //     {
+    //         _x = x;
+    //         _y = y;
+    //     }
+    //     public void setX(float x)
+    //     {
+    //         _x = x;
+    //     }
+    //     public void setY(float y)
+    //     {
+    //         _y = y;
+    //     }
+    //     public float getX()
+    //     {
+    //         return _x;
+    //     }
+    //     public float getY()
+    //     {
+    //         return _y;
+    //     }
+    // }
+    // class Line
+    // {
+    //          private Point   _A;
+    //          private Point _B;
+    //     public void setPointA(Point A)
+    //     {
+    //         _A = A;
+    //     }
+    //     public void setPointB(Point B)
+    //     {
+    //         _B = B;
+    //     }
+    //     public Point getPointA()
+    //     {
+    //         return _A;
+    //     }
+    //     public Point getPointB()
+    //     {
+    //         return _B;
+    //     }
+    // }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Soldie
+        }
+    }
+}
